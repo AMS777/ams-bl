07473fa64bfea8894a01ca14d318b4707af97bde
AMS777 <alberto@oscuro.es>
2018-03-21 16:56:19 +0100

    Add 'view-email-on-browser' to router to view emails on browser.

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,15 @@ $router->get('user', 'UserController@getUser');
 $router->post('user', 'UserController@createUser');
 $router->delete('user', 'UserController@deleteUser');
 $router->post('contact-message', 'MessagingController@contactMessage');
+
+//$router->group(['prefix' => 'view-email-on-browser'], function () use ($router) {
+//
+//    $router->get('/contact-message', function () {
+//
+//        return new App\Mail\ContactMessageEmail([
+//            'name' => 'Test Name',
+//            'email' => 'test@test.test',
+//            'message' => 'Test message.',
+//        ]);
+//    });
+//});
