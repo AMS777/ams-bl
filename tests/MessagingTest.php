b43be72bce022fc0d95264908aad368f7b45706b
AMS777 <alberto@oscuro.es>
2018-03-21 21:41:52 +0100

    Refactor MessagingTest.php to add setUp().

--- tests/MessagingTest.php
+++ tests/MessagingTest.php
@@ -1,7 +1,5 @@
 <?php
 
-use Laravel\Lumen\Testing\DatabaseMigrations;
-use Laravel\Lumen\Testing\DatabaseTransactions;
 use App\Helpers\HttpStatusCodes;
 
 class MessagingTest extends TestCase
@@ -15,18 +13,21 @@ class MessagingTest extends TestCase
         ],
     ]];
 
+    public function setUp()
+    {
+        parent::setUp();
+
+        config(['mail.driver' => 'log']);
+    }
+
     public function testSendContactMessage_Success()
     {
-        config(['mail.driver' => 'log']);
-
         $this->post('/contact-message', $this->contactMessage)
             ->seeStatusCode(HttpStatusCodes::SUCCESS_NO_CONTENT);
     }
 
     public function testSendContactMessage_ErrorNoData()
     {
-        config(['mail.driver' => 'log']);
-
         $this->post('/contact-message', [])
             ->seeStatusCode(HttpStatusCodes::CLIENT_ERROR_UNPROCESSABLE_ENTITY);
     }
