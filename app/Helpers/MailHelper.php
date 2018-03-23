3fd87c19cfa4258cb7ee510171f130ebaa259653
AMS777 <alberto@oscuro.es>
2018-03-23 10:19:09 +0100

    Arrange log messages from exceptions.

--- app/Helpers/MailHelper.php
+++ app/Helpers/MailHelper.php
@@ -23,21 +23,21 @@ class MailHelper
             Mail::to($emailTo)->bcc(env('MAIL_FROM_ADDRESS'))->send($emailContent);
 
         } catch(Swift_RfcComplianceException $e) {
-            Log::error($e->getMessage() . ' [Helpers/MailHelper.php->sendEmail(): catch(Swift_RfcComplianceException)]');
+            Log::error('[Helpers/MailHelper.php->sendEmail(): catch(Swift_RfcComplianceException)] ' . $e->getMessage());
 
             $errors = ['email' => [$e->getMessage()]];
 
             return ResponseHelper::getJsonApiErrorResponse($errors, HttpStatusCodes::CLIENT_ERROR_UNPROCESSABLE_ENTITY);
 
         } catch(Swift_TransportException $e) {
-            Log::error($e->getMessage() . ' [Helpers/MailHelper.php->sendEmail(): catch(Swift_TransportException)]');
+            Log::error('[Helpers/MailHelper.php->sendEmail(): catch(Swift_TransportException)] ' . $e->getMessage());
 
             $errors = ['email' => ['The contact message cannot be sent because of an error with the sendmail process.']];
 
             return ResponseHelper::getJsonApiErrorResponse($errors);
 
         } catch(Exception $e) {
-            Log::error($e->getMessage() . ' [Helpers/MailHelper.php->sendEmail(): catch(Exception)]');
+            Log::error('[Helpers/MailHelper.php->sendEmail(): catch(Exception)] ' . $e->getMessage());
 
             $errors = ['email' => ['The contact message cannot be sent because of an error sending the email.']];
 
